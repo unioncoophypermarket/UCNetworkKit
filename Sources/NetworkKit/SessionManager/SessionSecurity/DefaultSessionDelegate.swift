a7a600ba5e8a61095ac0657b1b8f02b0ee4f3976
Mahmoud Alaa <96684295+Ma7m0udalaa@users.noreply.github.com>
2025-01-27 08:57:55 +0200

    Handle URLSession Pinning Delegates

---
+++ Sources/NetworkKit/SessionManager/SessionSecurity/DefaultSessionDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultSessionDelegate.swift
+//  NetworkKit
+//
+//  Created by Mahmoud Alaa on 1/27/25.
+//
+
+import Foundation
+
+public
+class DefaultSessionDelegate: NSObject, URLSessionDelegate {
+    
+    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        // Default handling
+        completionHandler(.performDefaultHandling, nil)
+    }
+}
