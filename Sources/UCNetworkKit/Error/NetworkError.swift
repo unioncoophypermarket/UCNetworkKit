8898453e607134a9feecb269efd6ff95031466ca
Mahmoud Alaa <96684295+Ma7m0udalaa@users.noreply.github.com>
2026-01-07 14:57:11 +0200

    Optimizing Network Error

--- Sources/UCNetworkKit/Error/NetworkError.swift
+++ Sources/UCNetworkKit/Error/NetworkError.swift
@@ -7,28 +7,44 @@
 
 import Foundation
 
-public
-enum NetworkError: Error {
+public enum NetworkError: Error {
+
     case error(statusCode: Int, data: Data?)
     case notConnected
     case cancelled
     case generic(Error)
     case urlGeneration
-}
 
-
-extension NetworkError {
-    
-    public var isNotFoundError: Bool {
-        return hasStatusCode(404)
-    }
-    
-    public func hasStatusCode(_ codeError: Int) -> Bool {
+    public var errorCode: Int {
         switch self {
-        case let .error(code, _):
-            return code == codeError
-        default: return false
+        case let .error(statusCode, _):
+            return statusCode
+        case .notConnected:
+            return NSURLErrorNotConnectedToInternet
+        case .cancelled:
+            return NSURLErrorCancelled
+        case .urlGeneration:
+            return NSURLErrorBadURL
+        case let .generic(error):
+            return (error as NSError).code
         }
     }
-    
+
+    public var statusCode: Int? {
+        if case let .error(code, _) = self {
+            return code
+        }
+        return nil
+    }
+}
+
+extension NetworkError {
+
+    public var isNotFoundError: Bool {
+        self.statusCode == 404
+    }
+
+    public func hasStatusCode(_ code: Int) -> Bool {
+        self.statusCode == code
+    }
 }
--- Sources/UCNetworkKit/ErrorResolver/DataTransferErrorResolver/DataTransferErrorResolver.swift
+++ Sources/UCNetworkKit/ErrorResolver/DataTransferErrorResolver/DataTransferErrorResolver.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 public
 protocol DataTransferErrorResolver {
     func resolve(error: NetworkError) -> Error
